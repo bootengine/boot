98358a40b865ae21579a8b01471f33c1e01f08b0
mJehanno <mathob.jehanno@hotmail.fr>
2024-11-15 18:35:20 +0100

    feat(runner): adds the step runner
    the implementation is not complete, there are other check to do, other path to deal with

--- go.mod
+++ go.mod
@@ -23,9 +23,13 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/doug-martin/goqu/v9 v9.19.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/dylibso/observe-sdk/go v0.0.0-20240819160327-2d926c5d788a // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/extism/go-sdk v1.6.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20240805132620-81f5be970eca // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -40,11 +44,15 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tetratelabs/wabin v0.0.0-20230304001439-f6f874872834 // indirect
+	github.com/tetratelabs/wazero v1.8.1-0.20240916092830-1353ca24fef0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	golang.org/x/mod v0.20.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
 	modernc.org/libc v1.55.3 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -2,23 +2,32 @@ package runner
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
 	"slices"
+	"strings"
 
 	"github.com/bootengine/boot/internal/model"
+	"github.com/bootengine/boot/internal/usecase"
 	"github.com/charmbracelet/huh"
+	extism "github.com/extism/go-sdk"
 )
 
 type Runner struct {
 	ctx      context.Context
 	workflow model.Workflow
+	modCase  *usecase.ModuleUsecase
 }
 
 type ValueKey struct{}
 
-func NewRunner() *Runner {
+func NewRunner(use *usecase.ModuleUsecase) *Runner {
 	return &Runner{
-		ctx: context.Background(),
+		ctx:     context.Background(),
+		modCase: use,
 	}
 }
 
@@ -112,9 +121,92 @@ func (r *Runner) handleVars() error {
 func (r Runner) handleSteps() error {
 	for _, step := range r.workflow.Steps {
 		// find module
+		mod, err := r.modCase.RetrieveModule(r.ctx, step.Name)
+		if err != nil {
+			return err
+		}
 
+		// check type and action
 		fmt.Println(step)
+
+		manifest := extism.Manifest{
+
+			Wasm: []extism.Wasm{
+				extism.WasmFile{
+					Name: mod.Name,
+					Path: mod.Path,
+				},
+			},
+		}
+
+		config := extism.PluginConfig{
+			EnableWasi: true,
+		}
+
+		plugin, err := extism.NewPlugin(r.ctx, manifest, config, []extism.HostFunction{})
+		if err != nil {
+			return err
+		}
+		var params []byte
+		if step.Params != nil {
+			params, err = json.Marshal(step.Params)
+			if err != nil {
+				return err
+			}
+		}
+
+		// if action is folder_struct, need to get template before
+
+		exit, out, err := plugin.CallWithContext(r.ctx, string(step.Action), params)
+		if err != nil {
+			return err
+		}
+		switch mod.Type {
+		case model.FilerType, model.VCSType:
+			if exit != 0 {
+				errString := plugin.GetErrorWithContext(r.ctx)
+				return errors.New(errString)
+			}
+		// log success
+		case model.ModuleType:
+			cwd, err := func() (string, error) {
+				if step.CurrentWorkingDir != "" {
+					return step.CurrentWorkingDir, nil
+				}
+				if r.workflow.Config.CreateRoot {
+					return r.ctx.Value(ValueKey{}).(map[string]any)["project_name"].(string), nil
+				}
+				return os.Getwd()
+			}()
+			if err != nil {
+				return err
+			}
+			err = r.executeCommand(string(out), cwd)
+			if err != nil {
+				return err
+			}
+		}
+
 	}
 
 	return nil
+}
+
+func (r Runner) executeCommand(cmd string, cwd string) error {
+	if !r.workflow.Config.Unrestricted && !r.checkCommandContent(cmd) {
+		return fmt.Errorf("plugin is trying to execute a suspicious command: %s", cmd)
+	}
+
+	command := exec.CommandContext(r.ctx, cmd)
+	command.Stdin, command.Stdout, command.Stderr = os.Stdin, os.Stdout, os.Stderr
+
+	command.Dir = cwd
+	return command.Run()
+}
+
+func (r Runner) checkCommandContent(cmd string) bool {
+	if strings.Contains(cmd, "sudo") || strings.Contains(cmd, "rm") {
+		return false
+	}
+	return true
 }
