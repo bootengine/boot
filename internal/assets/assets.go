6cd1f58bb4fb3cacfb5fd7c6fcff6c49715e316d
mJehanno <mathob.jehanno@hotmail.fr>
2025-01-03 17:11:35 +0100

    docs(assets): adds godoc comment for assets package

--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -1,6 +1,9 @@
+// Package assets contains embedded assets
 package assets
 
 import "embed"
 
+// LicenseFS is an [embed.FS] with every embedded License files.
+//
 //go:embed licenses/*
 var LicenseFS embed.FS
